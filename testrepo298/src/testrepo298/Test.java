943ffc2a3fde448f2ad922bd218a2ed0a61bc29a
rahul <sambarirahul07@gmail.com>
2019-01-25 19:34:18 -0600

    Initial commit

---
+++ testrepo298/src/testrepo298/Test.java
@@ -0,0 +1,126 @@
+package testrepo298;
+
+
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Test {
+
+	
+	private static final String DB_CONNECTION = "jdbc:mysql://localhost:1521:newdb1";
+	private static final String DB_USER = "root";
+	private static final String DB_PASSWORD = "nikhil";
+
+	public static void main(String[] argv) {
+
+		try {
+
+			batchInsertRecordsIntoTable();
+
+		} catch (SQLException e) {
+
+			System.out.println(e.getMessage());
+
+		}
+
+	}
+
+	private static void batchInsertRecordsIntoTable() throws SQLException {
+
+		Connection dbConnection = null;
+		PreparedStatement preparedStatement = null;
+
+		String insertTableSQL = "INSERT INTO DBUSER"
+				+ "(USER_ID, USERNAME, CREATED_BY, CREATED_DATE) VALUES"
+				+ "(?,?,?,?)";
+
+		try {
+			dbConnection = getDBConnection();
+			preparedStatement = dbConnection.prepareStatement(insertTableSQL);
+
+			dbConnection.setAutoCommit(false);
+
+			preparedStatement.setInt(1, 101);
+			preparedStatement.setString(2, "mkyong101");
+			preparedStatement.setString(3, "system");
+			preparedStatement.setTimestamp(4, getCurrentTimeStamp());
+			preparedStatement.addBatch();
+
+			preparedStatement.setInt(1, 102);
+			preparedStatement.setString(2, "mkyong102");
+			preparedStatement.setString(3, "system");
+			preparedStatement.setTimestamp(4, getCurrentTimeStamp());
+			preparedStatement.addBatch();
+
+			preparedStatement.setInt(1, 103);
+			preparedStatement.setString(2, "mkyong103");
+			preparedStatement.setString(3, "system");
+			preparedStatement.setTimestamp(4, getCurrentTimeStamp());
+			preparedStatement.addBatch();
+
+			preparedStatement.executeBatch();
+
+			dbConnection.commit();
+
+			System.out.println("Record is inserted into DBUSER table!");
+
+		} catch (SQLException e) {
+
+			System.out.println(e.getMessage());
+			dbConnection.rollback();
+
+		} finally {
+
+			if (preparedStatement != null) {
+				preparedStatement.close();
+			}
+
+			if (dbConnection != null) {
+				dbConnection.close();
+			}
+
+		}
+
+	}
+
+	private static Connection getDBConnection() {
+
+		Connection dbConnection = null;
+
+		try {
+
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+		} catch (ClassNotFoundException e) {
+
+			System.out.println(e.getMessage());
+
+		}
+
+		try {
+
+			dbConnection = DriverManager.getConnection(
+                              DB_CONNECTION, DB_USER,DB_PASSWORD);
+			return dbConnection;
+
+		} catch (SQLException e) {
+
+			System.out.println(e.getMessage());
+
+		}
+
+		return dbConnection;
+
+	}
+
+	private static java.sql.Timestamp getCurrentTimeStamp() {
+
+		java.util.Date today = new java.util.Date();
+		return new java.sql.Timestamp(today.getTime());
+
+	}
+
+}
